5b0cc90cbb071822c1f6dccdfaab6aa63d511f5e
Andrey Kamyshanov <andrey@brokeree.com>
2019-04-08 22:09:55 +0300

    init

---
+++ net462-mvc21/Startup.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+// ReSharper disable once CheckNamespace
+namespace NSwagIssue
+{
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet, Route("values")]
+        public string[] GetValues() => new[] {"hello", "world"};
+    }
+
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddSwaggerDocument();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseMvc();
+            app.UseSwagger();
+        }
+    }
+}
